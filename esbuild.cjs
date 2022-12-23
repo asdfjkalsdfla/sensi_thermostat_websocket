bb09bde9bcb233c6e5d703730fffbd6a427d4ae3
asdfjkalsdfla <asdfjkalsdfla@asdfjkalsdfla.com>
2022-12-23 16:29:46 -0500

    fix esbuild

---
+++ esbuild.cjs
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const esbuild = require('esbuild')
+
+// Automatically exclude all node_modules from the bundled version
+const { nodeExternalsPlugin } = require('esbuild-node-externals')
+
+esbuild.build({
+  entryPoints: ['./src/index.ts'],
+  outdir: 'build/',
+  bundle: true,
+  minify: true,
+  platform: 'node',
+  format: 'esm',
+  sourcemap: true,
+  plugins: [nodeExternalsPlugin()]
+}).catch(() => process.exit(1))
