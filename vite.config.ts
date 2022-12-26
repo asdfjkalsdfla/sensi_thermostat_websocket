4649b4e14e8841c6fc151903abb3f11cb2d25c73
Tom Thumb <github@asdfjkalsdfla.com>
2022-12-26 15:21:02 -0500

    ensure all files are included in coverage report

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
+      all: true,
+      src: './src',
       reporter: ['text', 'json', 'html'],
       reportsDirectory: './test/coverageReports'
     },
