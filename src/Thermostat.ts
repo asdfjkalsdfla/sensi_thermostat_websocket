f8bf8c23586a0d6dd526ab0a1c42584aec36b1b7
Tom Thumb <github@asdfjkalsdfla.com>
2022-12-27 09:42:08 -0500

    need must last at least 2 minutes vs 3 minutes

--- src/Thermostat.ts
+++ src/Thermostat.ts
@@ -81,14 +81,14 @@ export class Thermostat {
     }
     // determine if the message indicates the HVAC is turning on
     if (!this.is_running && (stateUpdates?.demand_status?.cool >= 0 || stateUpdates?.demand_status?.heat >= 0)) {
-      this.remoteSensorTempAtHVACStart=this.remoteSensorTempLastRead;
+      this.remoteSensorTempAtHVACStart = this.remoteSensorTempLastRead;
     }
     const newState = _.merge(this.state, stateUpdates);
     this.state = newState;
   }
 
   async setThermostatTempToSensorTemp(sensorTemperature: number) {
-    // store temp read to use when system turns on    
+    // store temp read to use when system turns on
     this.remoteSensorTempLastRead = sensorTemperature;
 
     // CALCULATE: Determine the offset to apply
@@ -117,7 +117,7 @@ export class Thermostat {
 
     // CHECK: That the temp at sensor exceeds the change needed
     // This ensurers the sensor has changed at least 1 degree before changing the offset
-    if (this.remoteSensorTempAtHVACStart && this.is_running && Math.abs(this.remoteSensorTempAtHVACStart - sensorTemperature) < 1 ) {
+    if (this.remoteSensorTempAtHVACStart && this.is_running && Math.abs(this.remoteSensorTempAtHVACStart - sensorTemperature) < 1) {
       this.dateSinceOffsetNeedDetected = null; // if we no longer need the offset, set the offset need detection date to null
       console.log(`Offset not changed since HVAC is running and temp diff not greater than 1 (temp at start - ${this.remoteSensorTempAtHVACStart}; temp now - ${sensorTemperature})`);
       return;
@@ -154,9 +154,9 @@ export class Thermostat {
     }
 
     // CHECK: Ensure we've needed the offset for at last x minutes; this prevents changing this right before we get a new tempature reading from the thermostat
-    // Currently set to 3 minutes
+    // Currently set to 2 minutes
     const durationOfOffsetNeedDetected: number = (currentDate.valueOf() - this.dateSinceOffsetNeedDetected?.valueOf()) || 0;
-    if ((durationOfOffsetNeedDetected) < 3 * 60 * 1000) {
+    if ((durationOfOffsetNeedDetected) < 2 * 60 * 1000 + 2000) {
       console.log(`Offset not changed since the need was recently detected (need detected ${timeDeltaFormatter.format(durationOfOffsetNeedDetected / 1000 / 60)} min ago at ${this.dateSinceOffsetNeedDetected})`);
       return;
     }
